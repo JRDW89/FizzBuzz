553cc4813ef0a8c1ffe6599a6e3e3fb94ef2e64e
James.Warner <jrdw89@outlook.com>
2018-04-09 12:58:45 +0100

    FizzBuzz Commit

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzzer
+    {
+        public string generate(int i)
+        {
+            string str = "";
+
+            if (i % 3 == 0)
+            {
+                str = "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                str = "Buzz";
+            }
+            else if (i % 3 == 0 && i % 5 == 0)
+            {
+                str = "FizzBuzz"
+            }
+            else if(i % 3 != 0 && i % 5 != 0)
+            {
+                str = i.toString();
+            }
+
+            return str;
+        }
+    }
+}
---
+++ FizzBuzzer.test/FizzBuzzerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzer.test
+{
+    class FizzBuzzerTests
+    {
+            public void generateTest_IsDevisibleBy3()
+            {
+                    if(FizzBuzzer.generate(9) == "Fizz")
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+
+            }
+
+            public void generateTest_IsDevisibleBy5()
+            {
+                if (FizzBuzzer.generate(25) == "Buzz")
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+
+            }
+            public void generateTest_IsDevisibleBy3and5()
+            {
+                if (FizzBuzzer.generate(14) == "FizzBuzz")
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+
+            }
+            public void generateTest_IsDevisibleBy3and5()
+            {
+                if (FizzBuzzer.generate(2) == "2")
+                {
+                    Assert.Pass();
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+
+            }
+
+    }
+}
